c146d9127c1175bf106de0544c6edf7a2dbd922c
Hunter Stevens <onebree@gmail.com>
2016-02-23 19:22:06 -0500

    Add test

--- test/bowling_calculator_test.rb
+++ test/bowling_calculator_test.rb
@@ -7,4 +7,8 @@ describe BowlingCalculator do
   it "should begin with a score of 0" do
     calculator.score.must_equal(0)
   end
+
+  it 'should have a score of 3 when 3 pins are knocked down on the first ball' do
+    calculator.score.must_equal(3)
+  end
 end
